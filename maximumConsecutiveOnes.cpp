dd39b65a4e343a06947ab40e13b42211e29d3fca
siddhi-demo <sharmasiddhi2007@gmail.com>
2025-11-19 22:51:34 +0530

    maximumConsecutiveOnes

---
+++ maximumConsecutiveOnes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findMaxConsOnes(vector<int>&nums){
+    int cnt = 0;
+    int maxi = 0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i]==1){
+            cnt++;
+        }else{
+            cnt=0;
+        }
+        maxi = max(cnt, maxi);
+    }
+    return maxi;
+}
