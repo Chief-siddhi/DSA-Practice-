883e6228d4500cc77d6eb8f2955c22e825f5228b
siddhi-demo <sharmasiddhi2007@gmail.com>
2026-02-09 23:31:13 +0530

    leetcode 994

---
+++ leetcode994.cpp
@@ -0,0 +1,49 @@
+// class Solution {
+// public:
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+
+//         queue<pair<int,int>> q;
+//         int fresh = 0;
+
+//         // Count fresh oranges + push rotten ones
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(grid[i][j] == 2)
+//                     q.push({i, j});
+//                 else if(grid[i][j] == 1)
+//                     fresh++;
+//             }
+//         }
+
+//         // Directions: up, down, left, right
+//         vector<int> dx = {1, -1, 0, 0};
+//         vector<int> dy = {0, 0, 1, -1};
+
+//         int minutes = 0;
+
+//         while(!q.empty() && fresh > 0) {
+//             int size = q.size();
+//             minutes++;
+
+//             for(int i = 0; i < size; i++) {
+//                 auto [x, y] = q.front();
+//                 q.pop();
+
+//                 for(int d = 0; d < 4; d++) {
+//                     int nx = x + dx[d];
+//                     int ny = y + dy[d];
+
+//                     if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1) {
+//                         grid[nx][ny] = 2;
+//                         fresh--;
+//                         q.push({nx, ny});
+//                     }
+//                 }
+//             }
+//         }
+
+//         return fresh == 0 ? minutes : -1;
+//     }
+// };
