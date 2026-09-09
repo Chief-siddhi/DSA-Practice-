6bd5782f6cf29e877449877cc46ced1b67cfb8a9
SIDDHI SHARMA <sharmasiddhi2007@gmail.com>
2026-09-09 17:06:36 +0530

    reverse words in a string

---
+++ reverseWordInaGivenString.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+string reverseWords(string S){
+
+    string ans ="";
+    string temp = "";
+    for(int i=S.length()-1; i>=0; i++){
+        if(S[i] == '.'){
+            reverse(temp.begin(), temp.end());
+            ans = ans + temp +".";
+            temp = "";
+        }
+        else{
+            temp.push_back(S[i]);
+        }
+    }
+    reverse(temp.begin(), temp.end());
+    ans = ans + temp ;
+    return ans;;
+}
