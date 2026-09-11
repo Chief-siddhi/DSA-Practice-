2fb6c3d0eaed2cf73306e60edf6136ac9b1cea3b
SIDDHI SHARMA <sharmasiddhi2007@gmail.com>
2026-09-11 19:47:43 +0530

    koko eating bananas updated

--- kokoEatingBananas.cpp
+++ kokoEatingBananas.cpp
@@ -1,29 +1,46 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int minEatingBananas(vector<int> &piles, int h ){
-    int lo = 1, hi  = *max_element(piles.begin(), piles.end());
+long long hoursNeeded(vector<int>& piles, int k) {
+    long long hours = 0;
+
+    for (int p : piles) {
+        hours += (p + k - 1) / k;
+    }
+
+    return hours;
+}
+
+int minEatingBananas(vector<int>& piles, int h) {
+    int lo = 1;
+    int hi = *max_element(piles.begin(), piles.end());
+
     int ans = hi;
-    
-    while(lo <= high){
-        mid = lo+(hi-lo)/2;
-        
-        if(hoursNeeded(piles,mid)<=h){
-            ans= mid;
-            hi = mid-1;
-        }else{
-            lo = mid+1;
+
+    while (lo <= hi) {
+        int mid = lo + (hi - lo) / 2;
+
+        if (hoursNeeded(piles, mid) <= h) {
+            ans = mid;
+            hi = mid - 1;
+        }
+        else {
+            lo = mid + 1;
         }
     }
+
     return ans;
 }
-private:
-    long long hoursNeeded(vector<int>&piles, int k){
-        long long hours =0;
-        for(int p:piles){
-            hours += (p+k-1)/2;
-        return hours;
-        }
-    }
+
+int main() {
+    vector<int> piles = {3, 6, 7, 11};
+    int h = 8;
+
+    cout << "Minimum eating speed: "
+         << minEatingBananas(piles, h) << endl;
+
+    return 0;
+}
